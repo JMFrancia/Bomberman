a8f0786f6c2bf8f343c42b635168db135d17e997
franciaj <Jordan.Francia@viacom.com>
2019-11-17 14:14:22 -0500

    Added player controls; first draft level

---
+++ Assets/Bomberman.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomberman : MonoBehaviour
+{
+    public float speed = 20f;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput() {
+        float zAxis = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxis("Horizontal");
+
+
+        Vector3 delta = new Vector3(xAxis, 0f, zAxis) * speed;
+
+        if(delta == Vector3.zero) {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        } else {
+            rb.AddForce(delta, ForceMode.VelocityChange);
+        }
+
+        rb.velocity = delta;
+        //rb.AddForce(delta, ForceMode.VelocityChange);
+
+        //transform.position += delta;
+
+        //if(delta == Vector3.zero)
+    }
+
+}
