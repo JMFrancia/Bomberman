baa93c34cdbadca1dea14f77b67317b4abe83551
franciaj <Jordan.Francia@viacom.com>
2019-11-21 18:20:38 -0500

    Bombs blow up bombs

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -11,15 +11,30 @@ public class Bomb : MonoBehaviour
     [SerializeField] float explosionSpreadSpeed = .2f;
     [SerializeField] Vector3 explosionStartAdjustment = new Vector3(0f, -1f, 0f);
 
+    Coroutine fuseRoutine;
+
     private void Start()
     {
-        StartCoroutine(Set(fuseTime));
+        fuseRoutine = StartCoroutine(Set(fuseTime));
     }
 
     IEnumerator Set(float fuse) {
         yield return new WaitForSeconds(fuse);
+        Explode();
+    }
+
+    void Explode()
+    {
         GameObject exp = Instantiate(explosionPrefab, transform.position + explosionStartAdjustment, Quaternion.identity);
         exp.GetComponent<Explosion>().Init(explosionSpread, explosionSpreadSpeed);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == GlobalConstants.TagNames.BURST) {
+            StopCoroutine(fuseRoutine);
+            Explode();
+        }
     }
 }
